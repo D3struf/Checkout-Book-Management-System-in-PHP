91a04a3dfee9fa2b6bb1ba7ae5436a1852ee65cd
D3struf <johnpaul.monter@tup.edu.ph>
2024-06-02 22:26:48 +0800

    🔥Feature: Modify for admin

--- src/Admin/book.php
+++ src/Admin/book.php
@@ -1,10 +1,38 @@
+<?php
+    session_start();
+    include('../../config/db.php');
+
+    // Check if user is logged in
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: sign-in.php'); // Redirect to login page if not logged in
+        exit();
+    }
+
+    $user_id = $_SESSION['user_id'];
+
+    // Fetch user data
+    $query = "SELECT `account`.`EmailAddress`, `account`.`Password`, `account`.`AccountType`, `librarymember`.`FirstName`, `librarymember`.`LastName`, `librarymember`.`MembershipType`, `librarymember`.`ProfileImage`  
+    FROM `account` 
+    JOIN `librarymember` ON `account`.`AccountID` = `librarymember`.`AccountID` 
+    WHERE `account`.`EmailAddress` = '$user_id'";
+
+    $result = mysqli_query($conn, $query);
+
+    if ($result && mysqli_num_rows($result) == 1) {
+        $user = mysqli_fetch_assoc($result);
+    } else {
+        echo "User not found.";
+        exit();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library Management</title>
+    <title>Library Ease</title>
     <link rel="icon" type="image/x-icon" href="../../assets/img/logo.png">
 
     <!-- js -->
@@ -63,7 +91,7 @@
                     <ul id="navigation"
                         class=" flex flex-row gap-6 px-8 text-gray-400 font-medium max-sm:hidden max-sm:flex-col max-sm:px-4 max-sm:absolute max-sm:top-14 max-sm:bg-slate-800 max-sm:w-full max-sm:left-0 max-sm:gap-1 max-sm:pb-3 max-sm:rounded-b-lg">
                         <a class="py-2 px-3 rounded-md hover:bg-hover hover:text-primary_text" href="./index.php">
-                            <li>Report</li>
+                            <li>Dashboard</li>
                         </a>
                         <a class="py-2 px-3 bg-primary_blue rounded-md text-primary_text" href="./book.php">
                             <li>Books</li>
@@ -91,16 +119,39 @@
                     </div>
                 </div>
             </div>
-            <div class=" flex flex-row items-center gap-4 text-gray-400 max-sm:right-0 group">
-                <h4 class=" font-medium text-gray-400 group-hover:text-gray-100 group-hover:cursor:pointer">John Paul
-                    Monter</h4>
-                <button id="toggle" onclick="buttonToggle()" type="button"
-                    class="h-10 w-10 rounded-full cursor-pointer active:ring-offset-1 active:ring-1 active:ring-gray-200 group-hover:bg-hover">
-                    <a href="https://github.com/D3struf" target="_blank" rel="noopener noreferrer">
-                        <img class="h-10 w-10 rounded-full p-1" src="../../assets/img/icons8-github-64.png"
-                            alt="github-profile">
-                    </a>
+            <!-- Account Settings & Sign Out -->
+            <div class=" flex flex-row items-center gap-4 text-gray-400 max-sm:right-0 group ">
+                <h4 class=" font-medium text-gray-400 group-hover:text-gray-100 group-hover:cursor:pointer"><?php if ($user['Password'] == 'admin') {echo 'Admin';} else {echo htmlspecialchars($user['FirstName']); echo ' '; echo htmlspecialchars($user['LastName']);}?></h4>
+                <button id="toggle" onclick="buttonToggle()" type="button" class="h-10 w-10 rounded-full cursor-pointer active:ring-offset-1 active:ring-1 active:ring-gray-200">
+                    <?php if ($user['ProfileImage']): ?>
+                        <img src="data:image/jpeg;base64,<?php echo base64_encode($user['ProfileImage']); ?>" alt="Profile Image" class="h-8 w-8 mx-1 rounded-full">
+                    <?php else: ?>
+                        <svg class="text-gray-300 h-8 w-8 mx-1" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+                        </svg>
+                    <?php endif; ?>
                 </button>
+                <div id="settings" class="absolute bg-card rounded-md py-1 border border-shadow top-12 right-9 text-primary_text font-normal text-base leading-6 shadow-md hidden z-10">
+                    <ul class="flex flex-col">
+                        <a href="../account-setting.php" class="relative inline-flex items-center py-2 pl-4 pr-20 hover:bg-hover">
+                            <svg class="w-3 h-3 me-3"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path fill="#ffffff" d="M0 416c0 17.7 14.3 32 32 32l54.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 448c17.7 0 32-14.3 32-32s-14.3-32-32-32l-246.7 0c-12.3-28.3-40.5-48-73.3-48s-61 19.7-73.3 48L32 384c-17.7 0-32 14.3-32 32zm128 0a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM320 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm32-80c-32.8 0-61 19.7-73.3 48L32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l246.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48l54.7 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-54.7 0c-12.3-28.3-40.5-48-73.3-48zM192 128a32 32 0 1 1 0-64 32 32 0 1 1 0 64zm73.3-64C253 35.7 224.8 16 192 16s-61 19.7-73.3 48L32 64C14.3 64 0 78.3 0 96s14.3 32 32 32l86.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 128c17.7 0 32-14.3 32-32s-14.3-32-32-32L265.3 64z"/>
+                            </svg>
+                            <li>Account Settings</li></a>
+                        <a href="../about.php" class="relative inline-flex items-center py-2 pl-4 pr-20 hover:bg-hover">
+                            <svg class="w-3 h-3 me-3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="48px" height="48px">
+                                <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(5.33333,5.33333)"><path d="M24,4c-11.02771,0 -20,8.97229 -20,20c0,3.27532 0.86271,6.33485 2.26172,9.06445l-2.16797,7.76367c-0.50495,1.8034 1.27818,3.58449 3.08203,3.08008l7.76758,-2.16797c2.72769,1.39712 5.7836,2.25977 9.05664,2.25977c11.02771,0 20,-8.97229 20,-20c0,-11.02771 -8.97229,-20 -20,-20zM24,7c9.40629,0 17,7.59371 17,17c0,9.40629 -7.59371,17 -17,17c-3.00297,0 -5.80774,-0.78172 -8.25586,-2.14648c-0.34566,-0.19287 -0.75354,-0.24131 -1.13477,-0.13477l-7.38672,2.0625l2.0625,-7.38281c0.10655,-0.38122 0.05811,-0.7891 -0.13477,-1.13477c-1.36674,-2.4502 -2.15039,-5.25915 -2.15039,-8.26367c0,-9.40629 7.59371,-17 17,-17zM23.97656,12.97852c-0.82766,0.01293 -1.48843,0.69381 -1.47656,1.52148v12c-0.00765,0.54095 0.27656,1.04412 0.74381,1.31683c0.46725,0.27271 1.04514,0.27271 1.51238,0c0.46725,-0.27271 0.75146,-0.77588 0.74381,-1.31683v-12c0.00582,-0.40562 -0.15288,-0.7963 -0.43991,-1.08296c-0.28703,-0.28666 -0.67792,-0.44486 -1.08353,-0.43852zM24,31c-1.10457,0 -2,0.89543 -2,2c0,1.10457 0.89543,2 2,2c1.10457,0 2,-0.89543 2,-2c0,-1.10457 -0.89543,-2 -2,-2z"></path></g></g>
+                            </svg>
+                            <li class="">About</li>
+                        </a>
+                        <a href="../sign-in.php" class="relative inline-flex items-center py-2 pl-4 pr-20 hover:bg-hover">
+                            <svg class="w-4 h-4 me-2.5" data-name="Design Convert" id="Design_Convert" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" >
+                                <defs><style>.cls-1{fill:#ffffff;}</style></defs><title/><path class="cls-1" d="M55,28H34a1,1,0,0,1,0-2H55a1,1,0,0,1,0,2Z"/><path class="cls-1" d="M28,57a1,1,0,0,1-.45-.11L8.66,47.45A3,3,0,0,1,7,44.76V10a3,3,0,0,1,3-3h9a1,1,0,0,1,0,2H11.34l17.09,8.1A1,1,0,0,1,29,18V56a1,1,0,0,1-.47.85A1,1,0,0,1,28,57ZM9,10.11V44.76a1,1,0,0,0,.55.9L27,54.38V18.63Z"/><path class="cls-1" d="M47,37a1,1,0,0,1-.71-.29,1,1,0,0,1,0-1.42L54.59,27l-8.3-8.29a1,1,0,0,1,1.42-1.42l9,9a1,1,0,0,1,0,1.42l-9,9A1,1,0,0,1,47,37Z"/><path class="cls-1" d="M37,47H28a1,1,0,0,1,0-2h9a1,1,0,0,0,1-1V36a1,1,0,0,1,2,0v8A3,3,0,0,1,37,47Z"/><path class="cls-1" d="M39,19a1,1,0,0,1-1-1V10a1,1,0,0,0-1-1H15a1,1,0,0,1,0-2H37a3,3,0,0,1,3,3v8A1,1,0,0,1,39,19Z"/>
+                            </svg>
+                            <li>Sign out</li>
+                        </a>
+                    </ul>
+                </div>
             </div>
         </div>
     </header>
@@ -148,12 +199,13 @@
         </div>
 
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-3">
+            <!-- Search Book -->
             <form method="post" class=" pb-4 pr-4 bg-gray-900 flex flex-row items-center">
                 <label for="table-search" class="sr-only">Search</label>
                 <div class="relative pt-4 px-4">
                     <input type="text" id="table-search" name="search" autocomplete="off"
                         class="block h-10 ps-4 text-sm text-secondary_text align-center rounded-lg w-80 bg-gray-700 dark:border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                        placeholder="Search for book title">
+                        placeholder="Search for book info">
                 </div>
                 <button class=" h-10 mt-5 -ms-14 z-10 px-2 text-primary_text" name="submit">
                     <svg class="w-4 h-4  text-primary_text" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -201,7 +253,9 @@
                             $getQuery = "SELECT `book`.*, `checkout`.`ReturnDates`
                             FROM `book`
                             LEFT JOIN `checkout` ON `checkout`.`BookID` = `book`.`BookID`
-                            WHERE `book`.`Title` = '$search';";
+                            WHERE `book`.`Title` = '$search'
+                            OR `book`.`ISBN` = '$search'
+                            OR `book`.`Author` = '$search';";
                             
                             $result = mysqli_query($conn, $getQuery);
                             $total_rows = mysqli_num_rows($result);
